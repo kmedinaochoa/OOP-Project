06d1deed4913f250bdca18c2450090efe22a6ee9
Chris Szalwinski <cszalwinski@users.noreply.github.com>
2018-10-18 08:06:39 -0400

    ms1 initial draft fall 2018

---
+++ MS1/Date.h
@@ -0,0 +1,34 @@
+// Final Project Milestone 1
+//
+// Version 1.0
+// Date
+// Author
+// Description
+//
+//
+//
+//
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+/////////////////////////////////////////////////////////////////
+#ifndef AMA_DATE_H
+#define AMA_DATE_H
+
+namespace AMA {
+
+  class Date {
+
+
+     int mdays(int, int)const;
+ 
+  public:
+ 
+  };
+
+
+
+
+
+}
+#endif
